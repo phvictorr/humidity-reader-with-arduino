f1c4285bf9cfe9c249e825f7772bc5cf5655a7dc
Elian Damasceno <eliandamasceno460@gmail.com>
2025-07-12 19:25:07 -0300

    style: variaveis renomeadas

--- index.ino
+++ index.ino
@@ -1,61 +1,55 @@
 #include <Servo.h>
 
-#define p_servo 3
-#define buzzer 5
+#define PINO_SERVO 3
+#define PINO_BUZZER 5
 #define LIMITE_INFERIOR 23
 #define LIMITE_SUPERIOR 60
 
-#define potenciometro A0
 
 
 Servo servo;
 
 
-int valor_pot = 0;
-int pos = 0;
-int humidade = 24;
-int velocidade = 1;
-int tempo_motor = velocidade*10;
-int intervalo_beep = 2000/tempo_motor;
-int beep = intervalo_beep;
+int humidade = 0;
+
+int posicao_servo = 0;
+int velocidade_servo = 1;
+int tempo_servo = velocidade_servo*10;
+
+int intervalo_beep = (2000)/tempo_servo;  // para alterar o intervalo entre cada beep, altere apenas o valor entre parenteses
+int contador_beep = intervalo_beep;
 
 void setup() {
   
-  servo.attach(p_servo);
-  servo.write(pos);
+  servo.attach(PINO_SERVO);
+  servo.write(posicao_servo);
   Serial.begin(9600);
  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  valor_pot = analogRead(potenciometro);
-  humidade = map(valor_pot, 0, 1003,0,100);
-
   if (humidade < LIMITE_INFERIOR){
     // ativa o buzzer no com intervalo definido
-    if (beep== intervalo_beep){
-      tone(buzzer,440,500);
-      beep=0;
+    if (contador_beep == intervalo_beep){
+      tone(PINO_BUZZER , 440, 500);
+      contador_beep = 0;
     }
-    beep++;
+    contador_beep++;
 
     // coloca o motor na posicao de 90 graus
-    while(pos<90){
-      pos = pos + velocidade;
-      servo.write(pos);
-      delay(tempo_motor);
+    while(posicao_servo < 90){
+      posicao_servo = posicao_servo + velocidade_servo;
+      servo.write(posicao_servo);
+      delay(tempo_servo);
     }
-
-  
 
   }else if(humidade > LIMITE_SUPERIOR){
     //coloca o motor na posicao de 0 graus
-    while(pos>0){
-      pos = pos - velocidade;
-      servo.write(pos);
-      delay(tempo_motor);
+    while(posicao_servo > 0){
+      posicao_servo = posicao_servo - velocidade_servo;
+      servo.write(posicao_servo);
+      delay(tempo_servo);
     }
   }
 
